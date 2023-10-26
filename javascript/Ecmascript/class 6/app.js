262ec5a6a90143f417c2a6b5a300e219558f712b
ghulamhasnain007 <ghasnain668@gmail.com>
2023-10-26 10:00:15 +0500

    event

---
+++ javascript/Ecmascript/class 6/app.js
@@ -0,0 +1,33 @@
+// var btn = document.querySelector('#btn');
+// btn.addEventListener('click', greetUser)
+
+// function greetUser(){
+//     console.log("hello world")
+// }
+
+
+//bubbling if second parameter is false
+//catching if second parameter is true 
+// const ul = document.querySelector('.ul')
+
+// ul.addEventListener('click' , (event) => {
+//     if(event.target.tagName === 'IMG'){
+//         event.target.parentNode.remove()
+//         console.log('ul clicked')
+//     }
+//     else{
+//         console.log('Image nahi ha')
+//     }
+// },true)
+
+// var item = document.querySelector('.item')
+// item.addEventListener('click',(e)=>{
+// e.preventDefault()
+// console.log('google clicked')
+// })
+
+// var image2 = document.querySelector('li2')
+// image2.addEventListener('click' , (e) => {
+//     e.stopPropagation()
+//     console.log('image clicked')
+// })
