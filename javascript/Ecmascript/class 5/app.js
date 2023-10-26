e409762a021fc08f8ea79f7cb342a1efbe957bbe
ghulamhasnain007 <ghasnain668@gmail.com>
2023-10-26 08:41:02 +0500

    asyn await

---
+++ javascript/Ecmascript/class 5/app.js
@@ -0,0 +1,49 @@
+// console.log("hello world 1")
+// console.log("hello world 2")
+// console.log("hello world 3")
+
+// setTimeout(function(){
+// console.log("hello world 4")
+// },0)
+
+// console.log("hello world 5")
+
+// function rishta(bankbalance) {
+//     return new Promise((res, rej) => {
+//         if (bankbalance >= 100000) {
+//             console.log("Rishta Pakka")
+//         }
+//         else {
+//             console.log("Abba nahi maan rahay")
+//         }
+//     })
+// }
+// rishta(100000).then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// function getData(){
+//     axios.get('https://jsonplaceholder.typicode.com/users')
+//     .then((res)=>{
+//         console.log(res.data)
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
+// }
+// getData()
+// async function getData(){
+//     try{
+//         const res = await axios.get('https://jsonplaceholder.typicode.com/users') 
+//         console.log(res.data)
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+// getData()
+// console.log("hello world")
+
+
